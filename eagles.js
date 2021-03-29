aaa9198148071f239e5e68b86a2aaa23e3283fff
Aditya Deb Chowdhury <63070122+ADITYADEB072003@users.noreply.github.com>
2021-03-30 01:15:06 +0530

    code uploaded
    no problem till now... code to updates

---
+++ eagles.js
@@ -0,0 +1,241 @@
+var x = document.getElementById("myAudio");
+function playAudio() {
+    x.play();
+}
+function pauseAudio() {
+    x.pause();
+}
+function controls() {
+    x.controls = true;
+    x.load();
+}
+function vol1() {
+    x.volume = 0.5;
+}
+function vol11() {
+    x.volume = 1.0;
+}
+var w = document.getElementById("myAudio1");
+function play() {
+    w.play();
+}
+function pause() {
+    w.pause();
+}
+function con() {
+    w.volume = 0.5;
+}
+function con2() {
+    w.volume = 1.0;
+}
+function con3() {
+    w.controls = !0;
+}
+var e = document.getElementById("myAudio2");
+function play2() {
+    e.play();
+}
+function pause2() {
+    e.pause();
+}
+function vol2() {
+    e.volume = 0.5;
+}
+function vol22() {
+    e.volume = 1.0;
+}
+function con1() {
+    e.controls = !0;
+}
+var f = document.getElementById("myAudio3");
+function play3() {
+    f.play();
+}
+function pause3() {
+    f.pause();
+}
+function vol3() {
+    f.volume = 0.5;
+}
+function vol33() {
+    f.volume = 1.0;
+}
+function con2() {
+    f.controls = !0;
+}
+var r = document.getElementById("myAudio4");
+function play4() {
+    r.play();
+}
+function pause4() {
+    r.pause();
+}
+function vol4() {
+    r.volume = 0.5;
+}
+function vol44() {
+    r.volume = 1.0;
+}
+function con3() {
+    r.controls = !0;
+}
+var t = document.getElementById("myAudio5");
+function play5() {
+    t.play();
+}
+function pause5() {
+    t.pause();
+}
+function vol5() {
+    t.volume = 0.5;
+}
+function vol55() {
+    t.volume = 1.0;
+}
+function con4() {
+    t.controls = !0;
+}
+var y = document.getElementById("myAudio6");
+function play6() {
+    y.play();
+}
+function pause6() {
+    y.pause();
+}
+function vol6() {
+    y.volume = 0.5;
+}
+function vol66() {
+    y.volume = 1.0;
+}
+function con5() {
+    y.controls = !0;
+}
+var u = document.getElementById("myAudio7");
+function play7() {
+    u.play();
+}
+function pause7() {
+    u.pause();
+}
+function vol7() {
+    u.volume = 0.5;
+}
+function vol77() {
+    u.volume = 1.0;
+}
+function con6() {
+    u.controls = !0;
+}
+var i = document.getElementById("myAudio8");
+function play8() {
+    i.play();
+}
+function pause8() {
+    i.pause();
+}
+function vol8() {
+    i.volume = 0.5;
+}
+function vol88() {
+    i.volume = 1.0;
+}
+function con7() {
+    i.controls = !0;
+}
+var o = document.getElementById("myAudio9");
+function play9() {
+    o.play();
+}
+function pause9() {
+    o.pause();
+}
+function vol9() {
+    o.volume = 0.5;
+}
+function vol99() {
+    o.volume = 1.0;
+}
+function con8() {
+    o.controls = !0;
+}
+var p = document.getElementById("myAudio10");
+function play10() {
+    p.play();
+}
+function pause10() {
+    p.pause();
+}
+function vol10() {
+    p.volume = 0.5;
+}
+function vol100() {
+    p.volume = 1.0;
+}
+function con9() {
+    p.controls = !0;
+}
+var a = document.getElementById("myAudio11");
+function play11() {
+    a.play();
+}
+function pause11() {
+    a.pause();
+}
+function vol11() {
+    a.volume = 0.5;
+}
+function vol111() {
+    a.volume = 1.0;
+}
+function con10() {
+    a.controls = !0;
+}
+var s = document.getElementById("myAudio12");
+function play12() {
+    s.play();
+}
+function pause12() {
+    s.pause();
+}
+function vol12() {
+    s.volume = 0.5;
+}
+function vol120() {
+    s.volume = 1.0;
+}
+function con11() {
+    s.controls = !0;
+}
+var d = document.getElementById("myAudio13");
+function play13() {
+    d.play();
+}
+function pause13() {
+    d.pause();
+}
+function vol13() {
+    d.volume = 0.5;
+}
+function vol130() {
+    d.volume = 1.0;
+}
+function con12() {
+    d.controls = !0;
+}
+var g = document.getElementById("myAudio14");
+function play14() {
+    g.play();
+}
+function pause14() {
+    g.pause();
+}
+function vol14() {
+    g.volume = 0.5;
+}
+function vol140() {
+    g.volume = 1.0;
+}
+function con13() {
+    g.controls = !0;
+}
